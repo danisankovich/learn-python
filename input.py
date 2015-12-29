72183a50d3b006f9077cf51d4e070e1d9a542104
mikesankovich <misankovich@gmail.com>
2015-12-28 23:53:24 -0500

    input differences, long strings

--- input.py
+++ input.py
@@ -4,6 +4,6 @@
 # else:
 #     print "EXTERMINATE"
 #
-name = raw_input("What is your name? ")
-age = input("How old are you, " + name + "? ")
+name = raw_input("What is your name? ") #takes input and makes it string
+age = input("How old are you, " + name + "? ") #takes input as is. if you are trying to use a string, but do not type in quotes, it will throw error
 print "I see, " + name + ", you are " + str(age) + " years old."
--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-word = "Things"
-# print word[0:len(word)]
+# word = "Things"
+# # print word[0:len(word)]
+#
+# word+= "j"
+# print word
+# print repr("hello world") #this will print it with surrounding quotes.
 
-word+= "j"
-print word
+#backticks are also helpful when printing numbers
+# temp = 42
+# print "the temp is " + `temp`
+#or use repr()
+# print "the temp is " + repr(temp)
+
+#long strings
+print ''' this will go on
+for oh so long
+over so many lines
+cucumber'''
+#without the triple quotes, this would error. it prints with th eline breaks.
