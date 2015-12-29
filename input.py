bb66eb88b9067b66cc4cb108a60fd774a48de3cb
mikesankovich <misankovich@gmail.com>
2015-12-28 23:02:08 -0500

    math cmath, inputs

---
+++ input.py
@@ -0,0 +1,9 @@
+# y = input("The Meaning of Life: ");
+# if y == 42:
+#     print "you are correct"
+# else:
+#     print "EXTERMINATE"
+#
+name = raw_input("What is your name? ")
+age = input("How old are you, " + name + "? ")
+print "I see, " + name + ", you are " + str(age) + " years old."
---
+++ mathematics.py
@@ -0,0 +1,18 @@
+import math
+import cmath # used for complex math
+
+# from math import sqrt
+
+# x = math.floor(32.9) #round down
+# x = math.ceil(32.1) #round up
+# x = math.pi
+# x = sqrt(9) #import from you only use the function itself
+
+#you can use variables to refer to functions in modules.
+# foo = math.sqrt
+# x = foo(4)
+
+# x = math.sqrt(-1) #cannot perform on complex numbers like this
+# x = cmath.sqrt(-1) #just like overly long numbers end in an L, imaginary numbers end in j
+# x = 2j * 5j #you don't need the import cmath to do this math.
+# print x
