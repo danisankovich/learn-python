d06660f5c5f3ff6d590c013f4f8fc352fc352c26
Michael Sankovich <misankovich@gmail.com>
2016-01-06 01:17:53 -0800

    logical operators

--- python-course/test.py
+++ python-course/test.py
@@ -1,3 +1,17 @@
-dict1 = {"a": "hello", "b": 3}
-dict1['var'] = 66
-print(dict1)
+# x = True
+# y = False
+# z = y or x
+# print(z) # prints True => looks to see if either is true
+
+# x = True
+# y = False
+# z = y and x
+# print(z) # prints false => looks to see if both are true
+
+# x = True
+# x = not x
+# print(x) # prints false
+
+# x = False
+# x = not x
+# print(x) # prints true
