51d30baa60a6660fb154663aa5e2e224df8d816a
Michael <misankovich@gmail.com>
2016-01-11 23:40:29 -0500

    functions

---
+++ python-course/function.py
@@ -0,0 +1,13 @@
+# def Function_Name(arguments):
+#     statement/statements to execute
+
+# def func(things):
+#     return things
+# print(func("this and that and this that"))
+
+def argFunc(stuff = "big"):
+    "here"#only prints on language defined identifiers
+    return stuff
+# print(argFunc()) # returns "big"
+# print(argFunc("stiff")) # returns "stiff"
+print(argFunc.__doc__) #language defined identifiers (.__doc__)
